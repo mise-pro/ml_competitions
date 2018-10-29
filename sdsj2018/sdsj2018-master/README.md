0ed2f5216cf7e7320af612955248e016e937bd0a
Михаил <40423854+mise-pro@users.noreply.github.com>
2018-10-29 15:19:51 +0300

    1

--- sdsj2018/sdsj2018-master/README.md
+++ sdsj2018/sdsj2018-master/README.md
@@ -2,20 +2,40 @@
 
 Для работы необходим `make` и `docker`. Перед началом работы нужно скачать датасет в папку `data`.
 
+## Особенности решения
+
+* LightGBM с подбором гиперпараметров через hyperopt.
+* Mean target Encoding для категориальных фич.
+* Для 8-го датасета отбор фич через [BorutaPy](https://github.com/scikit-learn-contrib/boruta_py).
+* Лик от [bagxi](https://github.com/bagxi/sdsj2018_lightgbm_baseline).
+
+Так же есть, но не используются: Vowpal Wabbit, H2O AutoML.
+Скор на ЛБ: `5,30072`.
+
 ## Make-команды для работы с Docker :whale:
 
 `make download` - cкачать датасет в папку data.  
 `make train DATASET=1` - обучение модели на датасете с указанным номером [1-8].  
 `make predict DATASET=1` - валидация модели на датасете с указанным номером [1-8].  
+`make score` - валидация модели на всех датасетах и сохранение результата в папку scores.  
 `make docker-build` - сборка Docker-образа.  
 `make docker-push` - залить Docker-образ на Docker Hub.  
 `make run-bash` - запустить терминал в Docker-контейнере.  
-`make run-jupyter` - запустить Jupyter в Docker-контейнере.  
+`make run-jupyter` - запустить Jupyter в Docker-контейнере по адресу http://localhost:8888.  
 `make submission` - создать сабмит-файл в директории submissions.  
 
 ## Сборка своего Docker-образа
 
-1. Отредактировать `Makefile` и указать название образа на первой строчке `IMAGE=username/image`.  
-2. Отредактировать `Dockerfile`.
-3. Запустить сборку образа `make docker-build`.
-4. Залить Docker-образ на Docker Hub `make docker-push`.
+1. Зарегистрироваться на [Docker Hub](https://hub.docker.com/).
+2. Отредактировать [Makefile](https://github.com/tyz910/sdsj2018/blob/master/Makefile) и указать название образа на первой строчке `IMAGE=username/image`.  
+3. Отредактировать [Dockerfile](https://github.com/tyz910/sdsj2018/blob/master/Dockerfile) и добавить установку нужных пакетов.
+4. Запустить сборку образа `make docker-build`.
+5. Залить Docker-образ на Docker Hub `make docker-push`.
+6. Убедиться, что созданный репозиторий публичный (Public), а не приватный (Private). Приватность настраивается по ссылке `https://hub.docker.com/r/username/image/~/settings/`.
+
+## Запуск на Windows
+
+1. Установить [Docker](https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe).
+2. Установить [Make](http://gnuwin32.sourceforge.net/downlinks/make.php). И добавить его в [PATH](https://ru.stackoverflow.com/questions/153628/Как-добавить-путь-в-переменную-окружения-path-на-windows).
+3. Запустить Docker. В настройках выделить докеру нужное количество оперативной памяти.
+4. Запустить PowerShell от имени администратора. При выполнении make-команд дать разрешение на монтирование директории для докера.
--- sdsj2018/sdsj2018-master/lib/automl.py
+++ sdsj2018/sdsj2018-master/lib/automl.py
@@ -1,50 +1,69 @@
 import os
-import time
-import pickle
-from lib.util import timeit
+import pandas as pd
+import numpy as np
+from lib.util import timeit, Config
 from lib.read import read_df
 from lib.preprocess import preprocess
 from lib.model import train, predict, validate
+from typing import Optional
 
 
 class AutoML:
     def __init__(self, model_dir: str):
         os.makedirs(model_dir, exist_ok=True)
-        self.model_dir = model_dir
-        self.config = {
-            "start_time": time.time(),
-            "time_limit": int(os.environ.get("TIME_LIMIT", 5 * 60)),
-        }
+        self.config = Config(model_dir)
 
     def train(self, train_csv: str, mode: str):
+        self.config["task"] = "train"
         self.config["mode"] = mode
+        self.config.tmp_dir = self.config.model_dir + "/tmp"
+        os.makedirs(self.config.tmp_dir, exist_ok=True)
 
-        X = read_df(train_csv, self.config)
-        y = X["target"]
+        df = read_df(train_csv, self.config)
+        preprocess(df, self.config)
 
-        preprocess(X, self.config)
+        y = df["target"]
+        X = df.drop("target", axis=1)
+
         train(X, y, self.config)
 
-    def predict(self, test_csv: str, prediction_csv: str):
-        X = read_df(test_csv, self.config)
-        result = X[["line_id"]].copy()
+    def predict(self, test_csv: str, prediction_csv: str) -> (pd.DataFrame, Optional[np.float64]):
+        self.config["task"] = "predict"
+        self.config.tmp_dir = os.path.dirname(prediction_csv) + "/tmp"
+        os.makedirs(self.config.tmp_dir, exist_ok=True)
 
-        preprocess(X, self.config)
-        result["prediction"] = predict(X, self.config)
+        result = {
+            "line_id": [],
+            "prediction": [],
+        }
+
+        for X in pd.read_csv(
+                test_csv,
+                encoding="utf-8",
+                low_memory=False,
+                dtype=self.config["dtype"],
+                parse_dates=self.config["parse_dates"],
+                chunksize=self.config["nrows"]
+        ):
+            result["line_id"] += list(X["line_id"])
+            preprocess(X, self.config)
+            result["prediction"] += list(predict(X, self.config))
+
+        result = pd.DataFrame(result)
         result.to_csv(prediction_csv, index=False)
 
         target_csv = test_csv.replace("test", "test-target")
         if os.path.exists(target_csv):
-            validate(result, target_csv, self.config["mode"])
+            score = validate(result, target_csv, self.config["mode"])
+        else:
+            score = None
+
+        return result, score
 
     @timeit
     def save(self):
-        with open(os.path.join(self.model_dir, "config.pkl"), "wb") as f:
-            pickle.dump(self.config, f, protocol=pickle.HIGHEST_PROTOCOL)
+        self.config.save()
 
     @timeit
     def load(self):
-        with open(os.path.join(self.model_dir, "config.pkl"), "rb") as f:
-            config = pickle.load(f)
-
-        self.config = {**config, **self.config}
+        self.config.load()
